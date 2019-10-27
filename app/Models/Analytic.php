9bf97786019bc6f9367afad3f0b30b99da128243
Dương Mạnh Hoàng <manhhoang3151996@gmail.com>
2019-10-27 14:42:45 +0700

    Update user analytic

--- app/Models/Analytic.php
+++ app/Models/Analytic.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Analytic extends Model
 {
+    const WEEKLY = 1;
+    const MONTHLY = 2;
+    const YEAR = 3;
+    const ADVANCE = 4;
+    const TODAY_ACCESS = 1;
+    const YESTERDAY_ACCESS = 2;
+    const WEEK_AGO_ACCESS = 3;
+    const MONTH_AGO_ACCESS = 4;
+
     protected $fillable = [
         'page',
         'time',
