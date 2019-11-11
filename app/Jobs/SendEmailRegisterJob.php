63389dd8271f21b16b03f623451b82aa0ae7cabd
Dương Mạnh Hoàng <manhhoang3151996@gmail.com>
2019-11-11 23:06:04 +0700

    Update quequejob

---
+++ app/Jobs/SendEmailRegisterJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\User\RegiseterMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmailRegisterJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    private $input;
+
+    public function __construct($input)
+    {
+        $this->input = $input;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->input['email'])->send(new RegiseterMail($this->input, $this->input['remember_token']));
+    }
+}
