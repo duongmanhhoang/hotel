cd2ee2718d559d789b0efef5ac087219ca5f5abf
Dang Minh <minhku98@gmail.com>
2019-11-24 20:20:30 +0700

    user reader

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Contact\ContactRepository;
+use App\Repositories\ContactReader\ContactReaderRepository;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function __construct(ContactRepository $contactRepository)
+    public function __construct(ContactRepository $contactRepository, ContactReaderRepository $contactReaderRepository)
     {
         $this->contactRepo = $contactRepository;
+        $this->contactReaderRepo = $contactReaderRepository;
     }
 
     public function index()
@@ -30,6 +32,12 @@ class ContactController extends Controller
         $data = $this->contactRepo->getContactById($id);
 
         if($data == null) return redirect()->back()->with(['error' => 'Không tìm thấy dữ liệu']);
+
+        $userReader = $this->contactReaderRepo->findUserViaContactId($id);
+
+        if($userReader == null) {
+            $this->contactReaderRepo->insertReader($id);
+        }
 
         return view('admin.contact.detail', compact('data'));
     }
